b7b084e846f9942afd8e52947523031ac47e8ee9
hzy1754785321 <1754785321@qq.com>
2018-03-29 02:33:21 +0800

    Mario

---
+++ 脚本代码/menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+    int pos = 0;
+    public GameObject start;
+    public GameObject exit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(pos==0)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, -0.2f);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, exit.transform.position.y*1.6f );
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)|| Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pos = 1 - pos;
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            switch(pos)
+            {
+                case 0: SceneManager.LoadScene("mario");
+                        return;
+                case 1:
+                        Application.Quit();
+                        return;
+
+            }
+        }
+		
+	}
+}
